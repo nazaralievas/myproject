9293059a6c80930fa9e4b0149e0f9a559365d49d
nazaralievas <sadanaza4@gmail.com>
2026-01-31 19:39:56 +0600

    new02

--- core/models.py
+++ core/models.py
@@ -1,28 +1,37 @@
 from django.db import models
 
+
 class Movie(models.Model):
     name = models.CharField(max_length=255)
     is_published = models.BooleanField(default=True)
     description = models.TextField()
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to="images/")
     date_created = models.DateTimeField(auto_now_add=True)
+    name_lower = models.CharField(max_length=255, editable=False, null=True)
+
+    def save(self, *args, **kwargs):
+        self.name_lower = self.name.lower()
+        super(Movie, self).save(*args, **kwargs)
 
     class Meta:
-        verbose_name_plural = 'Фильмы'
+        verbose_name_plural = "Фильмы"
 
     def __str__(self):
-        return f'Фильм: {self.name}'
+        return f"Фильм: {self.name}"
+
 
 from django.contrib.auth.models import User
+
+
 class Comment(models.Model):
     text = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='comments')
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name="comments")
 
     class Meta:
-        ordering = ['-date_created']
-        verbose_name_plural = 'Комментарии'
+        ordering = ["-date_created"]
+        verbose_name_plural = "Комментарии"
 
     def __str__(self):
         return self.text
--- core/views.py
+++ core/views.py
@@ -4,48 +4,57 @@ from .forms import CommentForm
 
 
 def homepage(request):
-    movies = Movie.objects.filter(is_published=True)
-    return render(request, 'core/homepage.html', {'movies': movies})
+    search = request.GET.get("search")
+    if search:
+        movies = Movie.objects.filter(
+            is_published=True, name_lower__icontains=search.lower()
+        )
+    else:
+        movies = Movie.objects.filter(is_published=True)
+    return render(request, "core/homepage.html", {"movies": movies})
 
 
 def movie_detail(request, id):
     movie = Movie.objects.get(id=id, is_published=True)
 
-    if request.method == 'POST':
+    if request.method == "POST":
         form = CommentForm(request.POST)
         if form.is_valid():
             comment = form.save(commit=False)
             comment.movie = movie
             comment.user = request.user
             comment.save()
-            return redirect('movie_detail', id=movie.id)
+            return redirect("movie_detail", id=movie.id)
 
     form = CommentForm()
-    return render(request, 'core/movie_detail.html', {'movie': movie, 'form': form})
+    return render(request, "core/movie_detail.html", {"movie": movie, "form": form})
 
 
 # ----------- регистрация, авторизация, logout -------------
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+
 def register(request):
     context = {}
 
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('homepage')
-        context['form'] = form
+            return redirect("homepage")
+        context["form"] = form
     else:
-        context['form'] = UserCreationForm()
-    return render(request, 'core/register.html', context)
+        context["form"] = UserCreationForm()
+    return render(request, "core/register.html", context)
 
 
 from django.contrib import auth
+
+
 def login(request):
     if request.user.is_authenticated:
-        return redirect('homepage')
+        return redirect("homepage")
     else:
         context = {}
         if request.method == "POST":
@@ -53,12 +62,12 @@ def login(request):
             if form.is_valid():
                 user = form.get_user()
                 auth.login(request, user)
-                return redirect('homepage')
-            context['form'] = form
+                return redirect("homepage")
+            context["form"] = form
         context["form"] = auth.forms.AuthenticationForm()
-        return render(request, 'core/login.html', context)
+        return render(request, "core/login.html", context)
 
 
 def logout(request):
     auth.logout(request)
-    return redirect('homepage')
+    return redirect("homepage")
